54fdb624829fb59145ea90c7865e1a218a94f787
Michael Zhang <mzzhang2014@gmail.com>
2022-03-12 12:05:20 -0800

    typo

--- pages/about.vue
+++ pages/about.vue
@@ -12,7 +12,7 @@
 	<p>Originally a Caltech student club founded by Eduardo Beltrame in 2018, Sovereignty Club is now a 501(c)3 nonprofit organization open to all.  Our mission is to promote understanding of the world and its people.  We hold weekly meetings where we invite guest speakers to talk about geopolitics, history, governance, and many other topics.  We also encourage extensive discussion and debate, both with the speakers and among members, so that different perspectives can be heard and considered.  Previous guest speakers have included foreign policy expert Joshua Keating, Caltech professor Tracy Dennison, and Charter Cities Institute founder Mark Lutter.</p>
 
               <center>
-              <h4><a href="https://forms.gle/ZnhRc3ZvjX7fhVwU8">Join us!</a>.  It's fun.</h4>
+              <h4><a href="https://forms.gle/ZnhRc3ZvjX7fhVwU8">Join us!</a>  It's fun.</h4>
 	      <h4> To help us out, <a href="https://www.patreon.com/sovereigntyclub/">donate to our Patreon</a>.</h4>
               <h4> We meet every Wednesday at 6PM PST (and occasionally also at other times). </h4>
               <h4> To contact us, email clubsovereignty at gmail dot com </h4>
